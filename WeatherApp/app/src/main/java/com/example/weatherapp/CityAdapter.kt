0386ebf738f0bce746426757914f0f0ab1536a91
Brooke Claroni <brookeclaroni@Brookes-MacBook-Pro.local>
2020-11-05 02:48:36 -0500

    star functionality works on cities screen

--- WeatherApp/app/src/main/java/com/example/weatherapp/CityAdapter.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapp/CityAdapter.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat.startActivity
@@ -23,12 +24,13 @@ class CityAdapter (private val citySet: MutableSet<String>) : RecyclerView.Adapt
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         //get shared prefs
         val preferences = holder.card.context.getSharedPreferences("weather-app", Context.MODE_PRIVATE)
+        val savedCitySet = preferences.getStringSet("SAVED_CITIES", mutableSetOf())
         val imp = preferences.getBoolean("IMPERIAL", true)
 
         //retrieve the current result to add to the recycler view
         val currentCity = citySet.elementAt(position)
 
-        //fill in event, location, and artist
+        //fill in city name
         holder.cityName.text = currentCity
 
         doAsync {
@@ -42,8 +44,24 @@ class CityAdapter (private val citySet: MutableSet<String>) : RecyclerView.Adapt
             }
         }
 
-        holder.card.setOnClickListener {
+        //if star is grey and then the user clicks to save
+        holder.starOff.setOnClickListener{
+            //change the color, object data, and update shared preferences
+            holder.starOn.visibility = View.VISIBLE
+            savedCitySet?.add(currentCity)
+            preferences.edit().putStringSet("SAVED_CITIES", savedCitySet).apply()
+        }
 
+        //if star is yellow and then the user clicks to unsave
+        holder.starOn.setOnClickListener{
+            //change the color, object data, and update shared preferences
+            holder.starOn.visibility = View.INVISIBLE
+            savedCitySet?.remove(currentCity)
+            preferences.edit().putStringSet("SAVED_CITIES", savedCitySet).apply()
+        }
+
+        //when the card is clicked go to that city's landing page
+        holder.card.setOnClickListener {
             preferences
                 .edit()
                 .putString("CURR_CITY", currentCity)
@@ -64,5 +82,7 @@ class CityAdapter (private val citySet: MutableSet<String>) : RecyclerView.Adapt
         val cityName : TextView = itemView.findViewById(R.id.cityNameTextView)
         val cityTemp : TextView = itemView.findViewById(R.id.cityTempTextView)
         val card : CardView = itemView.findViewById(R.id.cardView)
+        val starOn : ImageButton = itemView.findViewById(R.id.starOnButton2)
+        val starOff : ImageButton = itemView.findViewById(R.id.starOffButton2)
     }
 }
