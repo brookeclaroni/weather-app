d4a78863f7eebb069cdbbeaf1429bfaf579b31c3
AisakaJerry <fengzeyuanasd@gmail.com>
2020-11-21 03:03:43 -0500

    Improved line chart design; Redesigned detail page layout.

--- WeatherApp/app/src/main/java/com/example/weatherapp/DetailsActivity.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapp/DetailsActivity.kt
@@ -2,6 +2,7 @@ package com.example.weatherapp
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -15,6 +16,7 @@ import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 import kotlinx.android.synthetic.main.activity_details.*
 import org.jetbrains.anko.doAsync
 
@@ -69,11 +71,11 @@ class DetailsActivity : AppCompatActivity() {
         day3CondView = findViewById(R.id.day3Condition)
         day4CondView = findViewById(R.id.day4Condition)
         day5CondView = findViewById(R.id.day5Condition)
-        day1TempView = findViewById(R.id.day1Temp)
-        day2TempView = findViewById(R.id.day2Temp)
-        day3TempView = findViewById(R.id.day3Temp)
-        day4TempView = findViewById(R.id.day4Temp)
-        day5TempView = findViewById(R.id.day5Temp)
+        //day1TempView = findViewById(R.id.day1Temp)
+        //day2TempView = findViewById(R.id.day2Temp)
+        //day3TempView = findViewById(R.id.day3Temp)
+        //day4TempView = findViewById(R.id.day4Temp)
+        //day5TempView = findViewById(R.id.day5Temp)
         day1AqiView = findViewById(R.id.day1Aqi)
         day2AqiView = findViewById(R.id.day2Aqi)
         day3AqiView = findViewById(R.id.day3Aqi)
@@ -114,6 +116,7 @@ class DetailsActivity : AppCompatActivity() {
                 day3CondView.text = fiveDayDetail[2].dayCondition
                 day4CondView.text = fiveDayDetail[3].dayCondition
                 day5CondView.text = fiveDayDetail[4].dayCondition
+                /*
                 if(imp) {
                     day1TempView.text =
                         fiveDayDetail[0].tempMax + "°F/" + fiveDayDetail[0].tempMin + "°F"
@@ -138,6 +141,8 @@ class DetailsActivity : AppCompatActivity() {
                     day5TempView.text =
                         fiveDayDetail[4].tempMax + "°C/" + fiveDayDetail[4].tempMin + "°C"
                 }
+
+                 */
 
                 day1AqiView.text = "AQI: " + fiveDayDetail[0].aqi
                 day2AqiView.text = "AQI: " + fiveDayDetail[1].aqi
@@ -343,44 +348,60 @@ class DetailsActivity : AppCompatActivity() {
                 //stop prog bar when networking is done
                 progBar.visibility=View.GONE
 
-                val entries = ArrayList<Entry>()
-                entries.add(Entry(1f, fiveDayDetail[0].tempMax.toFloat()))
-                entries.add(Entry(2f, fiveDayDetail[1].tempMax.toFloat()))
-                entries.add(Entry(3f, fiveDayDetail[2].tempMax.toFloat()))
-                entries.add(Entry(4f, fiveDayDetail[3].tempMax.toFloat()))
-                entries.add(Entry(5f, fiveDayDetail[4].tempMax.toFloat()))
+                val entriesMax = ArrayList<Entry>()
+                entriesMax.add(Entry(1f, fiveDayDetail[0].tempMax.toFloat()))
+                entriesMax.add(Entry(2f, fiveDayDetail[1].tempMax.toFloat()))
+                entriesMax.add(Entry(3f, fiveDayDetail[2].tempMax.toFloat()))
+                entriesMax.add(Entry(4f, fiveDayDetail[3].tempMax.toFloat()))
+                entriesMax.add(Entry(5f, fiveDayDetail[4].tempMax.toFloat()))
+                val entriesMin = ArrayList<Entry>()
+                entriesMin.add(Entry(1f, fiveDayDetail[0].tempMin.toFloat()))
+                entriesMin.add(Entry(2f, fiveDayDetail[1].tempMin.toFloat()))
+                entriesMin.add(Entry(3f, fiveDayDetail[2].tempMin.toFloat()))
+                entriesMin.add(Entry(4f, fiveDayDetail[3].tempMin.toFloat()))
+                entriesMin.add(Entry(5f, fiveDayDetail[4].tempMin.toFloat()))
 
-                val vl = LineDataSet(entries, "My Type")
+
+                val vl = LineDataSet(entriesMax, "TempMax")
+                val vl_2 = LineDataSet(entriesMin, "TempMin")
                 //Part4
-                vl.setDrawValues(false)
+                vl.valueTextSize = 15f
                 vl.setDrawFilled(true)
-                vl.lineWidth = 3f
+                vl.lineWidth = 4f
                 vl.fillColor = R.color.grey
-                vl.fillAlpha = R.color.red
-
-//Part5
+                // vl.fillAlpha = R.color.red
+                vl.setDrawCircles(true)
+                vl.mode = LineDataSet.Mode.HORIZONTAL_BEZIER
+                vl_2.valueTextSize = 15f
+                vl_2.setValueTextColors(listOf(1,1,1,1))
+                vl_2.setDrawFilled(true)
+                vl_2.lineWidth = 4f
+                vl_2.setDrawCircles(true)
+                vl_2.mode = LineDataSet.Mode.HORIZONTAL_BEZIER
+                val lineDataSets = listOf(vl, vl_2)
                 lineChartView.xAxis.labelRotationAngle = 0f
-//Part6
-                lineChartView.data = LineData(vl)
-//Part7
+
+                lineChartView.data = LineData(lineDataSets)
+
                 lineChartView.axisRight.isEnabled = false
                 lineChartView.setDrawBorders(false)
                 lineChartView.setScaleEnabled(false)
                 lineChartView.getDescription().setEnabled(false)
                 lineChartView.getLegend().setEnabled(false)
-                lineChartView.setHighlightPerDragEnabled(false)
+                lineChartView.isHighlightPerDragEnabled = false
+                lineChartView.isHighlightPerTapEnabled = false
                 // grid
                 val xAxis: XAxis = lineChartView.xAxis
                 xAxis.setDrawAxisLine(false)
                 xAxis.setDrawGridLines(false)
+                xAxis.isEnabled = false
                 val leftYAxis = lineChartView.axisLeft
                 leftYAxis.isEnabled = false
                 val rightYAxis = lineChartView.axisRight
                 rightYAxis.isEnabled = false
 
-                lineChartView.data.setDrawValues(false)
-
-
+                // lineChartView.data.setDrawValues(false)
+                lineChartView.invalidate()
             }
         }
 
